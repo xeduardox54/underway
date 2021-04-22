546e9b9c3052023b2b0472cce8104ff5530001cd
Eduardo <xeduardox54@hotmail.com>
2021-04-22 18:40:38 -0500

    Archivo App sincronizado

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Cargas from './App/components/cargas';
+
+export default class App extends Component {
+
+  render() {
+
+    const Stack = createStackNavigator();
+
+    return(
+      <NavigationContainer>
+        <Stack.Navigator>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
